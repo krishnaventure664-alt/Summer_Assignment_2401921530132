a1e63c7e2b96626f58257965d43cd9ea8429b5f4
Krishna <krishnaventure664@gmail.com>
2026-06-20 14:40:02 +0530

    Completed week3 day3

---
+++ week3/day3/next_greater_element.java
@@ -0,0 +1,26 @@
+package week3.day3;
+import java.util.*;
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    Stack<Integer> st = new Stack<>();
+    HashMap<Integer,Integer> map = new HashMap<>();
+
+    for(int i=nums2.length-1;i>=0;i--){
+        while(!st.isEmpty() && st.peek()<nums2[i]){
+            st.pop();
+        }
+        int nge = st.isEmpty()?-1:st.peek();
+        map.put(nums2[i],nge);
+
+        st.push(nums2[i]);
+    }
+
+    int[] ans = new int[nums1.length];
+    for(int i=0;i<nums1.length;i++){
+        ans[i] = map.get(nums1[i]);
+    }
+
+    return ans;
+    }
+}
+
