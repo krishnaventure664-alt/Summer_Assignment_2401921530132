a34331fa6045479c6784464f3ee1debf091572d9
Krishna <krishnaventure664@gmail.com>
2026-06-27 13:50:26 +0530

    day1 completed

---
+++ week4/day1/invert_Binary_Tree.java
@@ -0,0 +1,31 @@
+package week4.day1;
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){}
+    TreeNode(int val){this.val = val;}
+    TreeNode(int val,TreeNode left,TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        // Swap left and right child
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        // Invert left and right subtrees
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
